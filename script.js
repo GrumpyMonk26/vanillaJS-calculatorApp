313a305d88a4841f632beb68a5282f0dc3de8b77
Jason Evans <evans.jason@hotmail.co.uk>
2023-05-15 13:15:27 +0100

    javascript added for functionality

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const buttonsEl = document.querySelectorAll('button');
+const inputFieldEl = document.getElementById('result');
 
+for (let i = 0; i < buttonsEl.length; i++) {
+  buttonsEl[i].addEventListener('click', () => {
+    const buttonValue = buttonsEl[i].textContent;
+    if (buttonValue === 'C') {
+      clearResults();
+    } else if (buttonValue === '=') {
+      calculateResult();
+    } else {
+      appendValue(buttonValue);
+    }
+  });
+}
+
+function clearResults() {
+  inputFieldEl.value = '';
+}
+
+function calculateResult() {
+  inputFieldEl.value = eval(inputFieldEl.value);
+}
+
+function appendValue(buttonValue) {
+  inputFieldEl.value += buttonValue;
+}
